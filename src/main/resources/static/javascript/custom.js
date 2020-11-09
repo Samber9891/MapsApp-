d993e94e76eac83716a8d78b4baf4f52a14c9587
Samber9891 <Slem0119@gmail.com>
2020-11-09 10:08:04 -0500

    set up javascript

---
+++ src/main/resources/static/javascript/custom.js
@@ -0,0 +1,10 @@
+
+      let map;
+      
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 32.7767, lng: -96.7970},
+          zoom: 10
+        });
+      }
+ 
