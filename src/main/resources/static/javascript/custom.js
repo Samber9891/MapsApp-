f8e3aa2b1ec637a14ecfa112c3842ae442f285ea
Samber9891 <Slem0119@gmail.com>
2020-11-10 07:55:36 -0500

    formatted marker

--- src/main/resources/static/javascript/custom.js
+++ src/main/resources/static/javascript/custom.js
@@ -1,10 +1,37 @@
 
       let map;
       
+      
+      
       function initMap() {
+        let image = {url: 'images/lonestar2.jpg', 
+        scaledSize: new google.maps.Size(50, 50)};
+
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 32.7767, lng: -96.7970},
-          zoom: 10
+          center: coords,
+          zoom: 10, 
+          scrollwheel:false,
         });
+
+            marker = new google.maps.Marker({
+            position: coords,
+            map: map,
+            icon : image,
+            draggable:true,
+            title:"Drag me!",
+            animation: google.maps.Animation.DROP, 
+        });
+
+        let contentString = '<h2>' + city + ', ' + state + '</h2>';
+
+        let infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+       
+        google.maps.event.addListener(marker, 'click', function() {
+           infowindow.open(map,marker);
+        });
+
+       
       }
  
